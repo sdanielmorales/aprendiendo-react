f97c101591476e3424dbea1fad9a16a5fe2f7f47
Santiago Morales <santiagomorales.reinoso@gmail.com>
2023-10-05 12:46:01 -0500

    update

--- projects/03-prueba-tecnica-junior/src/App.jsx
+++ projects/03-prueba-tecnica-junior/src/App.jsx
@@ -1,3 +1,28 @@
+import { useEffect, useState } from "react";
+
+const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`;
+
 export function App() {
-  return <div>App de gatitos</div>;
+  const [fact, setFact] = useState();
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then((res) => res.json())
+      .then((data) => {
+        const { fact } = data;
+        setFact(data);
+        const firstWord = fact.split(" ", 3).join(" ");
+        console.log(firstWord);
+        fetch(
+          `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+        )
+          .then((res) => res.json())
+          .then((response) => {
+            console.log(response);
+          });
+      });
+  }, []);
+
+  return <main>App de gatitos{fact && <p>{fact}</p>} </main>;
 }
